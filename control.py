fixed scaling bug

control.py
@@ -6,6 +6,7 @@ from logger import Logger
 import numpy as np
 from torus import torus_diff
 
+
 class PIDController:
     def __init__(self, Kp, Ki, Kd):
         self.Kp = Kp
@@ -13,31 +14,24 @@ class PIDController:
         self.Kd = Kd
         self.integral = 0
         self.prev_error = 0
-    
+
     def compute(self, position, setpoint, dt):
         error = torus_diff(setpoint, position)
         self.integral += error * dt
         derivative = (error - self.prev_error) / dt
-        output = (self.Kp * error 
-                + self.Ki * self.integral 
-                + self.Kd * derivative)
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
         self.prev_error = error
         return output
-    
+
     def reset(self):
         self.integral = 0
         self.prev_error = 0
 
-class TrajectoryFollower:
 
-    def __init__(self, 
-                 arm_sim, 
-                 controller1, 
-                 controller2, 
-                 kS = None, 
-                 kV = None, 
-                 kA = None, 
-                 kG = None):
+class TrajectoryFollower:
+    def __init__(
+        self, arm_sim, controller1, controller2, kS=None, kV=None, kA=None, kG=None
+    ):
         self.controller1 = controller1
         self.controller2 = controller2
         self.arm_sim = arm_sim
@@ -46,13 +40,13 @@ class TrajectoryFollower:
         self.kA = kA if kA is not None else calculate_kA(arm_sim)
         self.kG = kG if kG is not None else calulate_kG(arm_sim)
 
-    def follow_trajectory(self, trajectory, step, dt = 0.02):
-        """Output motor voltage based on trajectory 
+    def follow_trajectory(self, trajectory, step, dt=0.02):
+        """Output motor voltage based on trajectory
         with position, velocity, and acceleration
-        
+
         Parameters
         ----
-        trajectory: tuple of (positions, velocities, accelerations) 
+        trajectory: tuple of (positions, velocities, accelerations)
             where each is a list of (t1, t2) pairs for each time step
         step: current time step index
         dt: time step duration in seconds"""
@@ -61,58 +55,67 @@ class TrajectoryFollower:
         a_setpoint = trajectory.accelerations[step]
 
         upper_pid = self.controller1.compute(
-            self.arm_sim.upper_arm.position, p_setpoint[0], dt)
+            self.arm_sim.upper_arm.position, p_setpoint[0], dt
+        )
         forearm_pid = self.controller2.compute(
-            self.arm_sim.forearm.position, p_setpoint[1], dt)
+            self.arm_sim.forearm.position, p_setpoint[1], dt
+        )
 
         upper_ff = v_setpoint[0] * self.kV[0] + a_setpoint[0] * self.kA[0]
         forearm_ff = v_setpoint[1] * self.kV[1] + a_setpoint[1] * self.kA[1]
 
-        upper_gravity_ff = math.cos(
-            self.arm_sim.upper_arm.position) * self.kG[0]
-        forearm_gravity_ff = math.cos(
-            self.arm_sim.forearm.position 
-            + self.arm_sim.upper_arm.position) * self.kG[1]
+        upper_gravity_ff = math.cos(self.arm_sim.upper_arm.position) * self.kG[0]
+        forearm_gravity_ff = (
+            math.cos(self.arm_sim.forearm.position + self.arm_sim.upper_arm.position)
+            * self.kG[1]
+        )
 
         upper_voltage = upper_pid + upper_ff + upper_gravity_ff
         forearm_voltage = forearm_pid + forearm_ff + forearm_gravity_ff
 
+        Logger.recordData("upper_arm_pid", upper_pid)
+        Logger.recordData("upper_arm_ff", upper_ff)
+        Logger.recordData("upper_arm_gravity_ff", upper_gravity_ff)
+        Logger.recordData("upper_arm_voltage", upper_voltage)
+
+        Logger.recordData("forearm_pid", forearm_pid)
+        Logger.recordData("forearm_ff", forearm_ff)
+        Logger.recordData("forearm_gravity_ff", forearm_gravity_ff)
+        Logger.recordData("forearm_voltage", forearm_voltage)
+
         self.arm_sim.upper_arm.setVoltage(upper_voltage)
         self.arm_sim.forearm.setVoltage(forearm_voltage)
 
-    def follow_pos_trajectory(self, trajectory, step, dt = 0.02):
+    def follow_pos_trajectory(self, trajectory, step, dt=0.02):
         """Given a list of (t1_setpoint, t2_setpoint) pairs and a time step dt,
         compute and apply motor voltages to follow the trajectory."""
         setpoint = trajectory[step]
 
         upper_pid = self.controller1.compute(
-            self.arm_sim.upper_arm.position, setpoint[0], dt)
+            self.arm_sim.upper_arm.position, setpoint[0], dt
+        )
         forearm_pid = self.controller2.compute(
-            self.arm_sim.forearm.position, setpoint[1], dt)
+            self.arm_sim.forearm.position, setpoint[1], dt
+        )
 
         current = np.array(trajectory[step])
-        next = (
-            np.array(trajectory[step + 1])
-            if step < len(trajectory) - 1
-            else current
-        )
-        prev = np.array(trajectory[step-1]) if step > 0 else current
+        next = np.array(trajectory[step + 1]) if step < len(trajectory) - 1 else current
+        prev = np.array(trajectory[step - 1]) if step > 0 else current
         velocity = torus_diff(next, current) / dt
-        acceleration = (torus_diff(
-                            torus_diff(next, current), 
-                            torus_diff(current, prev)) 
-                        / (dt**2))
+        acceleration = torus_diff(
+            torus_diff(next, current), torus_diff(current, prev)
+        ) / (dt**2)
         if step >= len(trajectory) - 1:
             velocity = np.array((0.0, 0.0))
             acceleration = np.array((0.0, 0.0))
         upper_ff = velocity[0] * self.kV[0] + acceleration[0] * self.kA[0]
         forearm_ff = velocity[1] * self.kV[1] + acceleration[1] * self.kA[1]
 
-        upper_gravity_ff = math.cos(
-            self.arm_sim.upper_arm.position) * self.kG[0]
-        forearm_gravity_ff = math.cos(
-            self.arm_sim.forearm.position 
-            + self.arm_sim.upper_arm.position) * self.kG[1]
+        upper_gravity_ff = math.cos(self.arm_sim.upper_arm.position) * self.kG[0]
+        forearm_gravity_ff = (
+            math.cos(self.arm_sim.forearm.position + self.arm_sim.upper_arm.position)
+            * self.kG[1]
+        )
 
         Logger.recordData("upper_arm_gravity_ff", upper_gravity_ff)
 
@@ -126,30 +129,47 @@ class TrajectoryFollower:
         self.arm_sim.upper_arm.setVoltage(upper_voltage)
         self.arm_sim.forearm.setVoltage(forearm_voltage)
 
+
 def calulate_kG(arm_sim, g=9.81):
-    '''calculate a theoretical value of kG'''
+    """calculate a theoretical value of kG"""
     forearm = arm_sim.forearm
     upper_arm = arm_sim.upper_arm
-    kG1 = ((upper_arm.mass * upper_arm.dist_COM 
-                + forearm.mass * (upper_arm.length + forearm.dist_COM)) 
-            * g * upper_arm.resistance 
-            / (upper_arm.kE * upper_arm.gear_ratio))
-    kG2 = (forearm.mass * forearm.dist_COM * g * forearm.resistance 
-            / (forearm.kE * forearm.gear_ratio))
+    kG1 = (
+        (
+            upper_arm.mass * upper_arm.dist_COM
+            + forearm.mass * (upper_arm.length + forearm.dist_COM)
+        )
+        * g
+        * upper_arm.resistance
+        / (upper_arm.kE * upper_arm.gear_ratio)
+    )
+    kG2 = (
+        forearm.mass
+        * forearm.dist_COM
+        * g
+        * forearm.resistance
+        / (forearm.kE * forearm.gear_ratio)
+    )
     return kG1, kG2
 
+
 def calculate_kV(arm_sim):
-    '''calculate a theoretical value of kV'''
+    """calculate a theoretical value of kV"""
     upper_kV = arm_sim.upper_arm.gear_ratio * arm_sim.upper_arm.kE
     forearm_kV = arm_sim.forearm.gear_ratio * arm_sim.forearm.kE
     return upper_kV, forearm_kV
 
+
 def calculate_kA(arm_sim):
-    '''calculate a theoretical value of kA'''
-    upper_kA = ((arm_sim.upper_arm.moi 
-                    + arm_sim.upper_arm.length**2 * arm_sim.forearm.mass)
-                 * arm_sim.upper_arm.resistance 
-                / (arm_sim.upper_arm.kE * arm_sim.upper_arm.gear_ratio))
-    forearm_kA = (arm_sim.forearm.moi * arm_sim.forearm.resistance 
-                / (arm_sim.forearm.kE * arm_sim.forearm.gear_ratio))
+    """calculate a theoretical value of kA"""
+    upper_kA = (
+        (arm_sim.upper_arm.moi + arm_sim.upper_arm.length**2 * arm_sim.forearm.mass)
+        * arm_sim.upper_arm.resistance
+        / (arm_sim.upper_arm.kE * arm_sim.upper_arm.gear_ratio)
+    )
+    forearm_kA = (
+        arm_sim.forearm.moi
+        * arm_sim.forearm.resistance
+        / (arm_sim.forearm.kE * arm_sim.forearm.gear_ratio)
+    )
     return upper_kA, forearm_kA

trajectory.py
@@ -5,13 +5,13 @@ from logger import Logger
 import math
 
 import numpy as np
-from torus import torus_diff, torus_point_diff
+from torus import torus_diff, torus_tuple_wrap, torus_wrap, torus_tuple_diff
 
 
 @dataclass
 class Trajectory:
-    positions: np.ndarray      # shape (N, 2)
-    velocities: np.ndarray     # shape (N, 2)
+    positions: np.ndarray  # shape (N, 2)
+    velocities: np.ndarray  # shape (N, 2)
     accelerations: np.ndarray  # shape (N, 2)
 
     def __len__(self):
@@ -40,7 +40,8 @@ def linear_traj(path, dt=0.02):
 
 def trapezoidal_traj(waypoints, max_vel, max_accel, dt=0.02):
     """follow a path with trapezoidal velocity profile in configuration space
-    accelerates at max_accel until max_vel, cruises at max_vel, then decelerates at max_accel to stop at the end of the path
+    accelerates at max_accel until max_vel, cruises at max_vel, 
+    then decelerates at max_accel to stop at the end of the path
 
     Parameters
     ---
@@ -86,7 +87,8 @@ def trap_traj_endpts(p1, p2, max_velocities, max_accelerations, dt=0.02):
 
     slope = (p2[1] - p1[1]) / (p2[0] - p1[0]) if p2[1] != p2[0] else math.inf
 
-    # choose the more restrictive constraint between the two joints to ensure we don't exceed limits on either joint
+    # choose the more restrictive constraint between the two joints to ensure we don't 
+    # exceed limits on either joint
     if max_accelerations[1] / max_accelerations[0] > slope:
         ratio = 1 / math.sqrt(1 + slope**2)  # ratio between x and total components
         max_accel = max_accelerations[0] * ratio
@@ -98,7 +100,7 @@ def trap_traj_endpts(p1, p2, max_velocities, max_accelerations, dt=0.02):
         max_accel = max_accelerations[1] * ratio
         max_vel = max_velocities[1] * ratio
 
-    diff = torus_point_diff(p2, p1)
+    diff = torus_tuple_diff(p2, p1)
 
     # now consider it as 1D
     distance = np.linalg.norm(diff)
@@ -115,13 +117,15 @@ def trap_traj_endpts(p1, p2, max_velocities, max_accelerations, dt=0.02):
             velocity = max_accel * i * dt
             velocities.append(decompose_scalar(p1, p2, velocity))
             position = 0.5 * max_accel * (i * dt) ** 2
-            positions.append(decompose_scalar(p1, p2, position))
+            raw = decompose_scalar(p1, p2, position)
+            positions.append((torus_wrap(raw[0]), torus_wrap(raw[1])))
         for i in range(half_num_steps):
             accelerations.append(decompose_scalar(p1, p2, -max_accel))
             velocity = max_accel * (half_num_steps - i) * dt
             velocities.append(decompose_scalar(p1, p2, velocity))
             position = distance - 0.5 * max_accel * ((half_num_steps - i) * dt) ** 2
-            positions.append(decompose_scalar(p1, p2, position))
+            raw = decompose_scalar(p1, p2, position)
+            positions.append((torus_wrap(raw[0]), torus_wrap(raw[1])))
     else:
         print("trapezoid profile")
         # trapezoid profile
@@ -134,24 +138,21 @@ def trap_traj_endpts(p1, p2, max_velocities, max_accelerations, dt=0.02):
             velocity = max_accel * i * dt
             velocities.append(decompose_scalar(p1, p2, velocity))
             position = 0.5 * max_accel * (i * dt) ** 2
-            positions.append(decompose_scalar(p1, p2, position))
+            positions.append(torus_tuple_wrap(decompose_scalar(p1, p2, position)))
         # cruise steps
         for i in range(cruise_steps):
             accelerations.append(decompose_scalar(p1, p2, 0))
             velocity = max_vel
             velocities.append(decompose_scalar(p1, p2, velocity))
             position = accel_distance + max_vel * i * dt
-            positions.append(decompose_scalar(p1, p2, position))
+            positions.append(torus_tuple_wrap(decompose_scalar(p1, p2, position)))
         # deceleration steps
         for i in range(accel_steps):
             accelerations.append(decompose_scalar(p1, p2, -max_accel))
             velocity = max_vel - max_accel * i * dt
             velocities.append(decompose_scalar(p1, p2, velocity))
             position = distance - 0.5 * max_accel * ((accel_steps - i) * dt) ** 2
-            positions.append(decompose_scalar(p1, p2, position))
-
-        Logger.recordData("cruise_distance", cruise_distance)
-        Logger.recordData("accel_distance", accel_distance)
+            positions.append(torus_tuple_wrap(decompose_scalar(p1, p2, position)))
 
     return (positions, velocities, accelerations)
 
@@ -165,7 +166,7 @@ def decompose_scalar(p1, p2, scalar):
     p2 : ending point in configuration space np array of joint angles
     scalar: scalar value to decompose
     returns a tuple of (x_component, y_component)"""
-    diff = torus_point_diff(p2, p1)
+    diff = torus_tuple_diff(p2, p1)
     distance = np.linalg.norm(diff)
     x_comp = p1[0] + scalar * diff[0] / distance
     y_comp = p1[1] + scalar * diff[1] / distance